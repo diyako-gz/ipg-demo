a4e3bd121ee0b90b84edda7ec6674c75f16371ae
amir-gz <amirgz98@gmail.com>
2025-02-01 20:54:13 +0330

    all pages demo mood ready

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { CodesubmitComponent } from "./components/codesubmit/codesubmit.component";
 import { ResualtPageComponent } from "./components/resualt-page/resualt-page.component";
 import { BasketUserComponent } from './components/basket-user/basket-user.component';
+import { InfoPageComponent } from './components/info-page/info-page.component';
+
 
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CodesubmitComponent, ResualtPageComponent, BasketUserComponent],
+  imports: [RouterOutlet, CodesubmitComponent, ResualtPageComponent, BasketUserComponent,InfoPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
